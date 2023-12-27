ea019fdde8bb78885a0f3cbb5c55396a89557109
Sutanu1234 <berasutanu02@gmail.com>
2023-12-27 10:48:18 +0530

    Update b.dart
    Update
    
    Co-Authored-By: Hbera1234 <154952447+Hbera1234@users.noreply.github.com>

--- lib/b.dart
+++ lib/b.dart
@@ -2,5 +2,5 @@ import 'package:flutter/material.dart';
 import 'package:new_travel_app/screens/home_screen.dart';
 import 'package:new_travel_app/screens/info_screen.dart';
 import 'style/app_style.dart';
-import 'style/size_config.dart';
+
 
